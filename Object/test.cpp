d102cfcc4f27632e0617db69091d98577397473a
riteme <riteme2013@gmail.com>
2015-05-01 17:39:52 +0800

    update

--- Object/test.cpp
+++ Object/test.cpp
@@ -1,33 +1,38 @@
+/**
+ * Copyright 2015 riteme
+ */
+
 #include <iostream>
-using namespace std;
+using std::cout;
+using std::endl;
 
 class A;
 
 class Object {
-  public:
-	virtual A GetA();
-}; // class Object
+ public:
+  virtual A GetA();
+};  // class Object
 
 class A: public Object {
-  public:
-	int i = 100;
-	int GetInt() {
-		return i;
-	}
-	virtual A GetA() {
-		return *this;
-	}
-}; // class A:public Object
+ public:
+  int i = 100;
+  int GetInt() {
+    return i;
+  }
+  virtual A GetA() {
+    return *this;
+  }
+};  // class A:public Object
 
 A Object::GetA() {
-	return this->GetA();
+  return this->GetA();
 }
 
 int main(int argc, char *argv[]) {
-	A a;
-	Object obj;
-	a.i = 101;
-	obj = a;
-	cout << obj.GetA().GetInt() << endl;
-	return 0;
-} //function main
+  A a;
+  Object obj;
+  a.i = 101;
+  obj = a;
+  cout << obj.GetA().GetInt() << endl;
+  return 0;
+}  // function main
--- TemplatePointer/test.cpp
+++ TemplatePointer/test.cpp
@@ -1,77 +1,70 @@
 /**
- * 4.30.2015
+ * Copyright 2015 riteme
  * Just for fun.
  * class ptr;
  * template class ptrT
  * ~
  */
 #include <iostream>
-using namespace std;
+using std::cout;
+using std::endl;
 
 struct A {
-	int a = 0;
-}; // struct A
+  int a = 0;
+};  // struct A
 
 class ptr {
-	A *m_p = nullptr;
-  public:
-	ptr() = default;
-	~ptr() = default;
-	ptr(A *p): m_p(p) {
+  A *m_p = nullptr;
 
-	}
-	ptr &operator=(A *value) {
-		this->m_p = value;
-		return *this;
-	}
+ public:
+  ptr() = default;
+  ~ptr() = default;
+  explicit ptr(A *p): m_p(p) {}
+  ptr &operator=(A *value) {
+    this->m_p = value;
+    return *this;
+  }
 
-	operator A*() {
-		return this->m_p;
-	}
-	A **operator&() {
-		return &m_p;
-	}
-	A &operator*() {
-		return *this->m_p;
-	}
-	A *operator->() {
-		return this->m_p;
-	}
-}; // class ptr
+  operator A*() {
+    return this->m_p;
+  }
+  A &operator*() {
+    return *this->m_p;
+  }
+  A *operator->() {
+    return this->m_p;
+  }
+};  // class ptr
 
 template<typename T>
 class ptrT {
-	T *m_p = nullptr;
-  public:
-	ptrT() = default;
-	~ptrT() = default;
-	ptrT(T *p): m_p(p) {
+  T *m_p = nullptr;
 
-	}
-	ptrT &operator=(T *value) {
-		this->m_p = value;
-		return *this;
-	}
+ public:
+  ptrT() = default;
+  ~ptrT() = default;
+  explicit ptrT(T *p): m_p(p) {}
+  ptrT &operator=(T *value) {
+    this->m_p = value;
+    return *this;
+  }
 
-	operator T*() {
-		return this->m_p;
-	}
-	T **operator&() {
-		return &m_p;
-	}
-	T &operator*() {
-		return *this->m_p;
-	}
-	T *operator->() {
-		return this->m_p;
-	}
-}; // class ptrT
+  operator T*() {
+    return this->m_p;
+  }
+  T &operator*() {
+    return *this->m_p;
+  }
+  T *operator->() {
+    return this->m_p;
+  }
+};  // class ptrT
 
 int main(int argc, char **argv) {
-	ptrT<A> p;
-	p = new A;
-	p->a = 1;
-	cout << p->a << endl;
-	delete p;
-	return 0;
-} //function main
+  ptrT<A> p;
+  p = new A;
+  p->a = 1;
+  cout << p->a << endl;
+  delete p;
+  return 0;
+}  // function main
