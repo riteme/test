1a21b126f9b45b674a867e2554e5297ec7007099
riteme <riteme@qq.com>
2015-10-14 18:41:40 +0800

    2015-10-14 backup 1

--- oi/Code/homework/2015-10-13/9-2/main.cpp
+++ oi/Code/homework/2015-10-13/9-2/main.cpp
@@ -1,11 +1,26 @@
+#include <cmath>
 #include <iostream>
 
 using namespace std;
 
-int main(){
-	ios::sync_with_stdio(false);
-	
-	
-	
-	return 0;
+typedef unsigned long long Number;
+
+// out of range...
+
+int main() {
+    ios::sync_with_stdio(false);
+
+    Number target = 1ULL;
+    Number n;
+    unsigned count = 1;
+    cin >> n;
+
+    while (target % n != 0) {
+        target = target * 10ULL + 1ULL;
+        count++;
+    }  // while
+
+    cout << count << endl;
+
+    return 0;
 }
