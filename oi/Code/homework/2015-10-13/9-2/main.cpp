0a3b51d44556b058c20feda7e950b4b3587b722b
riteme <riteme@qq.com>
2015-10-15 18:40:16 +0800

    2015-10-15 backup 1

--- oi/Code/homework/2015-10-13/9-2/main.cpp
+++ oi/Code/homework/2015-10-13/9-2/main.cpp
@@ -1,26 +1,63 @@
 #include <cmath>
+#include <cassert>
 #include <iostream>
 
 using namespace std;
 
-typedef unsigned long long Number;
+bool IsRepeatOf(int n, int i);
+int Length(int n);
 
-// out of range...
-
-int main() {
+int main(/*int argc, char *argv[]*/) {
     ios::sync_with_stdio(false);
 
-    Number target = 1ULL;
-    Number n;
-    unsigned count = 1;
+    int n;
     cin >> n;
 
-    while (target % n != 0) {
-        target = target * 10ULL + 1ULL;
-        count++;
+    int upload = 0;
+    int mult = 0;
+    int cnt = 0;
+
+    // Not right...
+    while (!IsRepeatOf(upload, 1)) {
+        for (int i = 1; i <= 9; i++) {
+            mult = n * i + upload;
+
+            if (mult % 10 == 1) {
+                upload = mult / 10;
+                cnt++;
+                break;
+            }
+        }  // for
+
+        // cout << "ERROR" << endl;
+        // return -1;
+        assert(mult % 10 == 1);
     }  // while
 
-    cout << count << endl;
+    cout << cnt + Length(upload) << endl;
 
     return 0;
+}  // function main
+
+bool IsRepeatOf(int n, int i) {
+    if (n == 0) { return false; }
+
+    while (n > 0) {
+        if (n % 10 != i) { return false; }
+
+        n /= 10;
+    }  // while
+
+    return true;
+}
+
+int Length(int n) {
+    int cnt = 0;
+
+    while (n > 0) {
+        cnt++;
+        n /= 10;
+    }  // while
+
+    return cnt;
 }
