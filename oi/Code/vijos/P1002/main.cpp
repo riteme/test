363016ea5621db6ffe933dbcc141ebf9eba49e81
riteme <1412803389@qq.com>
2015-11-11 18:28:24 +0800

    AC vijos P1002

--- oi/Code/vijos/P1002/main.cpp
+++ oi/Code/vijos/P1002/main.cpp
@@ -7,12 +7,12 @@
 using namespace std;
 
 #define MMAX 100
-#define LMAX 1000000
+#define LMAX 100000
 
 static int l, s, t, m;
-static int stone[MMAX + 10];
-static vector<int> f(LMAX + 10, INT_MAX);
-static vector<int> a(LMAX + 10, 0);
+static vector<int> stone(MMAX + 10);
+static vector<char> f(LMAX + 10, MMAX + 10);
+static vector<char> a(LMAX + 10, 0);
 static vector<bool> marked(LMAX + 10, false);
 
 int main() {
@@ -24,6 +24,7 @@ int main() {
 
     int cnt = 0;
     if (s == t) {
+        // Special judge
         for (int i = 1; i <= m; i++) {
             if (stone[i] % s == 0) { cnt++; }
         }  // for
@@ -34,20 +35,31 @@ int main() {
         int k = s * t;
         int offest = 0;
 
-        for (int i = 1; i <= m; i++) {
+        // Regard the endpoint as stone in order to process it.
+        stone[m + 1] = l;
+
+        for (int i = 1; i <= m + 1; i++) {
+            // Distance to last stone.
             int dist = stone[i] - offest - stone[i - 1];
 
-            if (dist > k) { offest += k; }
+            // If distance is greater than k, we can move this stone.
+            if (dist > k) offest += dist - k;
 
             stone[i] -= offest;
             a[stone[i]] = 1;
         }  // for
 
+        // However, the endpoint is not a stone.
+        a[stone[m + 1]] = 0;
+
+        // Array "marked" means to whether indicate a position is able to reach.
         marked[0] = true;
+
+        // There's no stone at the beginning.
         f[0] = 0;
-        for (int i = 0; i <= l - s - offest; i++) {
+        for (int i = 0; i <= l - offest; i++) {
             if (marked[i]) {
-                for (int j = i + s; j <= i + t and j <= l - offest; j++) {
+                for (int j = i + s; j <= i + t; j++) {
                     marked[j] = true;
 
                     if (f[i] + a[j] < f[j]) { f[j] = f[i] + a[j]; }
@@ -56,6 +68,11 @@ int main() {
         }  // for
 
         cnt = f[l - offest];
+
+        // Find the minimum stones.
+        for (int i = l - offest + 1; i <= l - offest + t; i++) {
+            if (f[i] < cnt) { cnt = f[i]; }
+        }  // for
     }
 
     cout << cnt;
