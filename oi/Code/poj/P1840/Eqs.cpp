7cf7a31152f1b7f6df3ece1bd7d1c216b71e44ae
riteme <1412803389@qq.com>
2015-11-20 13:51:34 +0800

    Add some comments to P1840

--- oi/Code/poj/P1840/Eqs.cpp
+++ oi/Code/poj/P1840/Eqs.cpp
@@ -1,3 +1,5 @@
+// AC
+
 #include <cstring>
 #include <cstdlib>
 #include <climits>
@@ -10,50 +12,47 @@
 
 using namespace std;
 
+// hash函数的取模值，10..07系列中最快的一个 +20%
 #define MOD 1000007
+// 状态数的最大值
 #define MAXN 1030302
 
+// 表示计算结果
 struct Result {
     Result() : value(0), count(0) {}
     Result(int _v, int _c) : value(_v), count(_c) {}
 
     int value;
-    int count;
+    int count; // 用于计数有多少个算式的值是value
 
     bool operator==(const int lhs) const { return value == lhs; }
 };  // struct Result
 
+// 被迫手动实现单链表，严重怀疑POJ在STL里下了毒... +50%
 template <typename T>
 struct Node {
     T value;
     Node *next;
-    // unsigned next;
 };  // struct Node
 
 typedef unsigned hash_t;
 typedef Node<Result> NodeType;
 
-// typedef list<Result>::iterator iterator_t;
-// static list<Result> hb[MOD];
-
-// typedef vector<Result>::iterator iterator_t;
-// static vector<Result> hb[MOD];
-static NodeType *hb[MOD];
-static NodeType memory[MAXN + 1];
-static int n = 1;
+static NodeType *hb[MOD]; // 散列表
+static NodeType memory[MAXN + 1]; // 优化：尽可能将单链表的内存分配到连续的空间上，降低cache miss +25%
+static int n = 1; // 计算使用过的内存
 
 static int a1, a2, a3, a4, a5;
 
-NodeType *list_allocate();
-void list_push(NodeType **node, int v);
-NodeType *list_find(NodeType *node, int v);
+NodeType *list_allocate();// 分配内存，其实就是在memory中拿出一个元素...
+void list_push(NodeType **node, int v); // 在指定节点的前面插入新的节点，若为NULL，则替换
+NodeType *list_find(NodeType *node, int v); // 查找节点
+
 void increase(int v);
 int count(int v);
 
 inline hash_t h(int v) {
     return (v >= 0 ? v : -v) % MOD;
-    // int n = v >= 0 ? v : -v;
-    // return (n % MOD + n / MOD) % MOD;
 }
 
 inline int compute(int x1, int x2, int x3) {
@@ -96,14 +95,6 @@ NodeType *list_find(NodeType *node, int v) {
 }
 
 /*inline*/ void increase(int v) {
-    // hash_t hx = h(v);
-    // iterator_t iter = std::find(hb[hx].begin(), hb[hx].end(), v);
-
-    // if (iter == hb[hx].end())
-    //     hb[hx].push_back(Result(v, 1));
-    // else
-    //     iter->count++;
-
     hash_t u = h(v);
     NodeType *iter = list_find(hb[u], v);
 
@@ -114,10 +105,6 @@ NodeType *list_find(NodeType *node, int v) {
 }
 
 /*inline*/ int count(int v) {
-    // for (iterator_t beg = hb[u].begin(); beg != hb[u].end(); beg++) {
-    //     if (v == beg->value) cnt += beg->count;
-    // }  // for
-
     hash_t u = h(v);
 
     NodeType *iter = list_find(hb[u], v);
@@ -138,6 +125,7 @@ int main(/*int argc, char *argv[]*/) {
     //             if (x2 != 0)
     //                 for (int x3 = -50; x3 <= 50; x3++)
     //                     if (x3 != 0) increase(compute(x1, x2, x3));
+    // 手动优化：使循环更紧凑，减低cache miss +50%
     for (int x1 = 1; x1 <= 50; x1++)
         for (int x2 = 1; x2 <= 50; x2++)
             for (int x3 = 1; x3 <= 50; x3++) {
@@ -156,6 +144,7 @@ int main(/*int argc, char *argv[]*/) {
     //     if (x1 != 0)
     //         for (int x2 = -50; x2 <= 50; x2++)
     //             if (x2 != 0) cnt += count(compute(x1, x2));
+    // 优化同上
     int cnt = 0;
     for (int x1 = 1; x1 <= 50; x1++)
         for (int x2 = 1; x2 <= 50; x2++) {
