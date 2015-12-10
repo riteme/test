bc58000c2ec3a7e0b5eb3bc3d40f6bb55fa593de
riteme <1412803389@qq.com>
2015-12-10 17:48:26 +0800

    AC Codevs P2598 'Levenshtein Distance'

--- oi/Code/codevs/P2598/main.cpp
+++ oi/Code/codevs/P2598/main.cpp
@@ -1,5 +1,17 @@
 // 编辑距离问题
-// WA
+
+// 插入操作与删除操作可以互换，为简便计算将插入视为删除
+// f[i][j]: 子串a[1..i]与子串b[1..j]的编辑距离
+// 则：
+// f[0][0] = 0, 无需对齐
+// f[0][j] = f[j][0] = j, 只能依靠删除j个字符来对齐
+// f[i][j] = min{f[i - 1][j] + 1,  删除a子串的一个字符
+//               f[i][j - 1] + 1,  删除b子串的一个字符
+//               f[i - 1][j - 1] + g(i, j)  将最后一个字符进行替换操作
+//              }
+//  注：
+//  g(i, j): 如果a[i]与b[j]相同，表示此处无需替换，返回0
+//           否则返回1
 
 #include <string>
 #include <iostream>
@@ -9,40 +21,22 @@ using namespace std;
 
 #define MAX_LENGTH 4000
 
-template <typename T>
-T abs(const T &v) {
-    return v >= 0 ? v : -v;
-}
-
 static string a, b;
-static int maxlen, minlen;
-static int dlen;
 static int f[MAX_LENGTH + 10][MAX_LENGTH + 10];
 
 void initialize();
 void output();
 
+inline int is_equal(int i, int j) { return a[i - 1] == b[j - 1] ? 0 : 1; }
+
 int main() {
     ios::sync_with_stdio(false);
     initialize();
 
-    string &m = a.size() < b.size() ? b : a;
-    string &n = a.size() >= b.size() ? b : a;
-
-    for (int j = 1; j <= minlen; j++) {
-        if (m[j - 1] == n[j - 1])
-            f[0][j] = f[0][j - 1];
-        else
-            f[0][j] = f[0][j - 1] + 1;
-    }  // for
-
-    for (int i = 1; i <= dlen; i++) {
-        for (int j = i + 1; j <= minlen + i; j++) {
-            int v = 0;
-
-            if (m[j - 1] != n[j - i - 1]) v = 1;
-
-            f[i][j] = min(f[i][j - 1] + v, f[i - 1][j - 1] + 1);
+    for (int i = 1; i <= a.size(); i++) {
+        for (int j = 1; j <= b.size(); j++) {
+            f[i][j] = min(min(f[i - 1][j] + 1, f[i][j - 1] + 1),
+                          f[i - 1][j - 1] + is_equal(i, j));
         }  // for
     }      // for
 
@@ -53,18 +47,12 @@ int main() {
 void initialize() {
     cin >> a >> b;
 
-    dlen = abs(int(a.size()) - int(b.size()));
-    maxlen = max(a.size(), b.size());
-    minlen = min(a.size(), b.size());
+    f[0][0] = 0;
 
-    for (int i = 0; i <= dlen; i++) { f[i][i] = i; }  // for
+    for (int i = 1; i <= MAX_LENGTH; i++) {
+        f[0][i] = i;
+        f[i][0] = i;
+    }  // for
 }
 
-void output() {
-    // for (int i = 0; i <= dlen; i++) {
-    //     for (int j = 1; j <= maxlen; j++) { cout << f[i][j] << " "; }  // for
-    //     cout << endl;
-    // }  // for
-
-    cout << f[dlen][maxlen];
-}
+void output() { cout << f[a.size()][b.size()]; }
