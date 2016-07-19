9620532711ea4d532a5e9701a07183739064ad5c
riteme <1412803389@qq.com>
2016-07-19 21:26:32 +0800

    Completed Iterative FFT

--- oi/Code/algs/FT/IterativeFFT.cpp
+++ oi/Code/algs/FT/IterativeFFT.cpp
@@ -20,21 +20,9 @@ static Complex iX[NMAX];
 static Complex rx[NMAX];
 static Complex aux[NMAX];
 
-inline void quick_dft(size_t left, size_t right, Complex *output,
-                      bool reverse = false) {
+static void fft(size_t left, size_t right, Complex *input, Complex *output,
+                bool reverse = false) {
     size_t n = right - left + 1;
-    size_t mid = (left + right) / 2;
-    Complex wn = exp(Complex(0, (reverse ? -1 : 1) * 2 * M_PI / n));
-    Complex w = Complex(1);
-    for (size_t k = 0; k < n / 2; k++) {
-        Complex t = output[mid + k + 1] * w;
-        output[mid + k + 1] = output[left + k] - t;
-        output[left + k] = output[left + k] + t;
-        w *= wn;
-    }  // for
-}
-
-inline void radering(size_t n, Complex *input, Complex *output) {
     output[0] = input[0];
     size_t rev = 0;
     for (size_t i = 1; i < n; i++) {
@@ -46,18 +34,21 @@ inline void radering(size_t n, Complex *input, Complex *output) {
         rev += k;
         output[rev] = input[i];
     }  // for
-}
 
-static void fft(size_t left, size_t right, Complex *input, Complex *output,
-                bool reverse = false) {
-    size_t n = right - left + 1;
-    radering(n, input, output);
+    for (size_t s = 2; s <= n; s <<= 1) {
+        Complex wn = my_exp((reverse ? -1.0 : 1.0) * 2.0 * M_PI / s);
+        for (size_t i = 0; i < n; i += s) {
+            size_t mid = i + (s >> 1);
+            Complex w = Complex(1);
 
-    for (size_t s = 1; (1 << s) <= n; s++) {
-        for (size_t i = 0; i < n; i += (1 << s)) {
-            quick_dft(i, i + (1 << s) - 1, output, reverse);
-        }  // for
-    }      // for
+            for (size_t k = 0; k < s / 2; k++) {
+                Complex t = output[mid + k] * w;
+                output[mid + k] = output[i + k] - t;
+                output[i + k] = output[i + k] + t;
+                w *= wn;
+            }  // for
+        }      // for
+    }          // for
 }
 
 int main() {
--- oi/Code/uoj/P34/main-2.cpp
+++ oi/Code/uoj/P34/main-2.cpp
@@ -1,20 +1,104 @@
 /**
- * Recursive FFT
+ * Iterative FFT
  */
 
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <climits>
+#include <cctype>
 #include <vector>
-#include <complex>
+// #include <complex>
 #include <algorithm>
 
 using namespace std;
 
+#define BUFFERSIZE 10086
+static size_t _pos = BUFFERSIZE;
+static char _buffer[BUFFERSIZE];
+
+inline char _getchar() {
+    if (_pos == BUFFERSIZE) {
+        _pos = 0;
+        fread(_buffer, 1, BUFFERSIZE, stdin);
+    }
+    return _buffer[_pos++];
+}
+
+template <typename T>
+inline T read() {
+    T x = 0;
+    char c = _getchar();
+    while (!isdigit(c))
+        c = _getchar();
+    while (isdigit(c)) {
+        x = x * 10 + (c - '0');
+        c = _getchar();
+    }
+    return x;
+}
+
 #define NMAX 300000
 
-typedef complex<double> Complex;
+// typedef complex<double> Complex;
+
+struct Complex {
+    Complex() : real(0.0), imag(0.0) {}
+    Complex(const double &_real) : real(_real), imag(0.0) {}
+    Complex(const double &_real, const double &_imag)
+            : real(_real), imag(_imag) {}
+
+    double real, imag;
+
+    friend Complex operator+(const Complex &a, const Complex &b);
+    friend Complex operator-(const Complex &a, const Complex &b);
+    friend Complex operator*(const Complex &a, const Complex &b);
+    friend Complex operator/(const Complex &a, const double &b);
+
+    Complex &operator*=(const Complex &b) {
+        double re = real;
+        double im = imag;
+        real = (re * b.real - im * b.imag);
+        imag = (re * b.imag + im * b.real);
+
+        return *this;
+    }
+    Complex &operator/=(const double &b) {
+        real /= b;
+        imag /= b;
+
+        return *this;
+    }
+};  // struct Complex
+
+Complex operator+(const Complex &a, const Complex &b) {
+    Complex c = Complex(a.real + b.real, a.imag + b.imag);
+
+    return c;
+}
+
+Complex operator-(const Complex &a, const Complex &b) {
+    Complex c = Complex(a.real - b.real, a.imag - b.imag);
+
+    return c;
+}
+
+Complex operator*(const Complex &a, const Complex &b) {
+    Complex c = Complex(a.real * b.real - a.imag * b.imag,
+                        a.real * b.imag + a.imag * b.real);
+
+    return c;
+}
+
+Complex operator/(const Complex &a, const double &b) {
+    Complex c = Complex(a.real / b, a.imag / b);
+
+    return c;
+}
+
+Complex my_exp(const double &x) {
+    return Complex(cos(x), sin(x));
+}
 
 static int n, m;
 static int ra;
@@ -23,7 +107,6 @@ static Complex X1[NMAX + 10];
 static Complex x2[NMAX + 10];
 static Complex X2[NMAX + 10];
 static Complex x3[NMAX + 10];
-static Complex aux[NMAX + 10];
 
 inline int next_pow(int x) {
     int k = 1;
@@ -33,41 +116,27 @@ inline int next_pow(int x) {
 }
 
 static void initialize() {
-    scanf("%d%d", &n, &m);
+    // scanf("%d%d", &n, &m);
+    n = read<int>();
+    m = read<int>();
 
-    for (int i = 0; i <= n; i++) {
-        double v;
-        scanf("%lf", &v);
-        x1[i].real(v);
-    }
-    for (int i = 0; i <= m; i++) {
-        double v;
-        scanf("%lf", &v);
-        x2[i].real(v);
-    }
+    for (int i = 0; i <= n; i++)
+        // scanf("%lf", &x1[i].real);
+        x1[i].real = read<double>();
+    for (int i = 0; i <= m; i++)
+        // scanf("%lf", &x2[i].real);
+        x2[i].real = read<double>();
 
     ra = next_pow(n + m + 2);
 }
 
-inline void quick_dft(size_t left, size_t right, Complex *output,
-                      bool reverse = false) {
-    size_t n = right - left + 1;
-    size_t mid = (left + right) / 2;
-    Complex wn = exp(Complex(0, (reverse ? -1 : 1) * 2 * M_PI / n));
-    Complex w = Complex(1);
-    for (size_t k = 0; k < n / 2; k++) {
-        Complex t = output[mid + k + 1] * w;
-        output[mid + k + 1] = output[left + k] - t;
-        output[left + k] = output[left + k] + t;
-        w *= wn;
-    }  // for
-}
-
-inline void radering(size_t n, Complex *input, Complex *output) {
+static void fft(int left, int right, Complex *input, Complex *output,
+                bool reverse = false) {
+    int n = right - left + 1;
     output[0] = input[0];
-    size_t rev = 0;
-    for (size_t i = 1; i < n; i++) {
-        size_t k = n >> 1;
+    int rev = 0;
+    for (int i = 1; i < n; i++) {
+        int k = n >> 1;
         while (k <= rev) {
             rev -= k;
             k >>= 1;
@@ -75,18 +144,39 @@ inline void radering(size_t n, Complex *input, Complex *output) {
         rev += k;
         output[rev] = input[i];
     }  // for
+
+    for (int s = 2; s <= n; s <<= 1) {
+        // Caution please: slow floating operations
+        Complex wn = my_exp((reverse ? -1.0 : 1.0) * 2.0 * M_PI / s);
+        for (int i = 0; i < n; i += s) {
+            int mid = i + (s >> 1);
+            Complex w = Complex(1);
+            for (int k = 0; k < s / 2; k++) {
+                Complex t = output[mid + k] * w;
+                output[mid + k] = output[i + k] - t;
+                output[i + k] = output[i + k] + t;
+                w *= wn;
+            }  // for
+        }      // for
+    }          // for
 }
 
-static void fft(size_t left, size_t right, Complex *input, Complex *output,
-                bool reverse = false) {
-    size_t n = right - left + 1;
-    radering(n, input, output);
+const int BufferSize = 1180000;
 
-    for (size_t s = 1; (1 << s) <= n; s++) {
-        for (size_t i = 0; i < n; i += (1 << s)) {
-            quick_dft(i, i + (1 << s) - 1, output, reverse);
-        }  // for
-    }      // for
+char _BUFFER[BufferSize];
+char *out_tail = _BUFFER;
+
+inline void putint(int x) {
+    if (!x)
+        *out_tail++ = '0';
+    else {
+        char s_pool[6], *s_tail = s_pool;
+        while (x != 0)
+            *s_tail++ = x % 10 + '0', x /= 10;
+        while (s_tail-- != s_pool)
+            *out_tail++ = *s_tail;
+    }
+    *out_tail++ = ' ';
 }
 
 int main() {
@@ -101,7 +191,9 @@ int main() {
         x3[i] /= static_cast<double>(ra);
 
     for (int i = 0; i <= n + m; i++)
-        printf("%d ", int(x3[i].real() + 0.5));
+        // printf("%d ", int(x3[i].real + 0.5));
+        putint(int(x3[i].real + 0.5));
 
+    fwrite(_BUFFER, 1, out_tail - _BUFFER, stdout);
     return 0;
 }  // function main
