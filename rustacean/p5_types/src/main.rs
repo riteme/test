99405417fd71aadfe0b9e338aaa49233c3ba27c7
riteme <riteme@qq.com>
2020-12-21 11:26:02 +0800

    Finished p5_types

---
+++ rustacean/p5_types/src/main.rs
@@ -0,0 +1,16 @@
+type nanoSecond = u64;
+
+fn main() {
+    let dec = 65.4321f64;
+    let int = dec as u8;
+    let chr = int as char;
+    println!("{}", int);
+    println!("{}", chr);
+    println!("{}", 66.0f64 as nanoSecond as u8 as char);
+    println!("{}", -1i64 as u8);
+    println!("{}", -3 % 7);
+
+    let mut vec = Vec::new();
+    vec.push(1i32);
+    println!("{:?}", vec);
+}
